5bb005bc005b1f3e91ef907394267ef43c0c7e2e
jeri <jerimonski@gmail.com>
2025-10-21 19:28:29 -0300

    feat: adding chat box 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import "./App.css"
-
 function App() {
   return (
-    <main id='mainbox'>
-      <section>asdasdsad</section>
-      <section>asdasdsad</section>
+    <main className='chat'>
+      <section className='chat__header'>
+        <div className='chat__user-info'>
+          <h2 className='chat__title'>Open Chat</h2>
+          <span className='chat__username'>Chateando como: "username"</span>
+        </div>
+        <div className='chat__stats'> "Cantidad de mensajes o usuarios"</div>
+      </section>
+      <section className='chat__messages'>
+        <div className='chat__message-list'>HERE IS GONNA BE ALL MESSAGES</div>
+        <div>
+          <input className='chat__input-message' type='text' />
+          <button className='chat__send-button'>Send</button>
+        </div>
+      </section>
     </main>
   )
 }
