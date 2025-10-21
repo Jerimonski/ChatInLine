8319a456a8f35dfbc371254e762f618022264cfe
jeri <jerimonski@gmail.com>
2025-10-20 21:32:22 -0300

    base

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  return (
+    <main id='mainbox'>
+      <section>asdasdsad</section>
+      <section>asdasdsad</section>
+    </main>
+  )
+}
+
+export default App
