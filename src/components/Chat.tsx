4485f1617bb826f2a8215f34ccd67cfbdf967f94
jeri <jerimonski@gmail.com>
2025-10-31 15:20:06 -0300

    refactor; change a p for span

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -52,50 +52,45 @@ export default function Chat({ socket, username, room }: ChatProps) {
 
   return (
     <main className='chat'>
-      <section className='chat__header'>
-        <div className='chat__user-info'>
-          <h2 className='chat__title'>Open Chat</h2>
-          <span className='chat__username'>Chateando como: {username}</span>
-        </div>
-        <div className='chat__stats'>
-          <span className='chat__messages-count'>
-            Room: {room} "usuarios activos"
-          </span>
-        </div>
-      </section>
-      <section className='chat__messages'>
-        <div className='chat__message-list'>
-          {messageList.map((message, index) => {
-            return (
-              <div
-                key={index}
-                className={`flex flex-col p-2.5 mb-2 rounded-md w-[70%] sm:w-80 ${
-                  message.id === socket.id
-                    ? "bg-purple-800 self-end"
-                    : "bg-slate-800 self-start"
-                }`}
-              >
-                <p className='font-bold'>{message.author}</p>
-                <p>{message.message}</p>
-                <p className='text-sm font-light text-end'>{message.time}</p>
-              </div>
-            )
-          })}
-        </div>
-        <div className='chat__box-input-message'>
-          <input
-            className='chat__input-message'
-            value={currentMessage}
-            onChange={(e) => setCurrentMessage(e.target.value)}
-            placeholder='Ingresa un comentario'
-            onKeyDown={(e) => e.key === "Enter" && sendMessage()}
-            type='text'
-          />
-          <button className='chat__send-button' onClick={sendMessage}>
-            IC
-          </button>
-        </div>
-      </section>
+      <div className='chat__container'>
+        <section className='chat__header'>
+          <div className='chat__user-info'>
+            <h2 className='chat__title'>Open Chat</h2>
+            <span className='chat__username'>Chateando como: {username}</span>
+          </div>
+          <div className='chat__stats'>
+            <span className='chat__messages-count'>Room: {room}</span>
+          </div>
+        </section>
+        <section className='chat__messages'>
+          <div className='chat__message-list'>
+            {messageList.map((message, index) => {
+              return (
+                <div key={index} className='chat__message-text'>
+                  <p className='font-bold'>{message.author}</p>
+                  <p>{message.message}</p>
+                  <span className='text-sm font-light text-end'>
+                    {message.time}
+                  </span>
+                </div>
+              )
+            })}
+          </div>
+          <div className='chat__box-input-message'>
+            <input
+              className='chat__input-message'
+              value={currentMessage}
+              onChange={(e) => setCurrentMessage(e.target.value)}
+              placeholder='Ingresa un comentario'
+              onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+              type='text'
+            />
+            <button className='chat__send-button' onClick={sendMessage}>
+              IC
+            </button>
+          </div>
+        </section>
+      </div>
     </main>
   )
 }
