ce27f8097e207a086cbb135826e0a6d663744a42
jeri <jerimonski@gmail.com>
2026-01-20 18:09:12 -0300

    chore: import animate dependencie

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client"
 import "./css/style.css"
 import App from "./App.tsx"
+import "animate.css"
 
 createRoot(document.getElementById("root")!).render(
   <>
